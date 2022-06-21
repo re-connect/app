2c2089d3fda7df16c834feb7aaeca0ff155ddac8
Frédéric RAFFIN-CABOISSE <fraffinc@gmail.com>
2022-06-21 17:39:03 +0200

    feat: itl phone number management

---
+++ src/components/UI/PhoneInputField.tsx
@@ -0,0 +1,85 @@
+import { Stack } from 'native-base';
+import React, { useEffect, useRef, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { StyleSheet, View } from 'react-native';
+import PhoneInput from 'react-native-phone-number-input';
+import { colors } from '../../style';
+import { TextFieldProps } from './TextField';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+
+export type PhoneNumberType = Omit<TextFieldProps, 'fieldLabel'>;
+
+const PhoneInputField: React.FC<PhoneNumberType> = ({ handleChange, touched, value, error, okIcon }) => {
+  const { t } = useTranslation();
+
+  const phoneInput = useRef<PhoneInput>(null);
+  /*const [valid, setValid] = useState(true);
+
+  useEffect(() => {
+    if (value) {
+      const checkValid = phoneInput.current?.isValidNumber(value);
+      setValid(!!checkValid);
+    }
+  }, [value]);
+  */
+
+  const getRightIconName = () => {
+    return !!value && !!okIcon && !!touched && !error ? 'check' : 'times';
+  };
+
+  const getIconColor = () => {
+    if (okIcon) {
+      return !!value && !!touched && !error ? colors.green : colors.red;
+    }
+    return 'transparent';
+  };
+
+  const RightElement = () => {
+    return !touched ? null : <Icon name={getRightIconName()} style={{ ...styles.icon }} color={getIconColor()} />;
+  };
+
+  return (
+    <Stack mt={3} space={4} w="100%" backgroundColor={colors.white} borderRadius="24">
+      <View style={[styles.wrapper, !!error && { borderColor: colors.red }]}>
+        <PhoneInput
+          filterProps={{ placeholder: t('choose_a_country'), style: { width: '100%' } }}
+          placeholder={t('phone')}
+          ref={phoneInput}
+          defaultValue={value}
+          defaultCode="FR"
+          layout="first"
+          onChangeFormattedText={handleChange}
+          textContainerStyle={styles.textContainer}
+          containerStyle={styles.container}
+        />
+        <RightElement />
+      </View>
+    </Stack>
+  );
+};
+
+const styles = StyleSheet.create({
+  phoneIndicator: { fontSize: 18, marginLeft: 16 },
+  wrapper: {
+    flex: 1,
+    height: 50,
+    borderRadius: 30,
+    borderColor: colors.darkGray,
+    borderWidth: 1,
+    backgroundColor: '#fff',
+    overflow: 'hidden',
+  },
+  icon: {
+    position: 'absolute',
+    right: 16,
+    top: 16,
+  },
+  container: { height: 50 },
+  textContainer: {
+    backgroundColor: colors.white,
+    height: 60,
+    paddingTop: 8,
+  },
+});
+
+export default PhoneInputField;
--- src/helpers/forms/createBeneficiaryShape.ts
+++ src/helpers/forms/createBeneficiaryShape.ts
@@ -4,28 +4,26 @@ import {
   addMinAndMaxNotEmpty,
   addOnlyLetters,
   getEmailSchema,
+  getItlPhoneSchema,
   getPasswordConfirmSchema,
-  getPhoneSchema,
-  getStringDateSchema
+  getStringDateSchema,
 } from './yupHelpers';
 
-export default (): Yup.ObjectSchema<any> => Yup.object().shape({
-  first_name: addOnlyLetters(addMinAndMaxNotEmpty(Yup.string(), 'first_name', 2, 254)),
-  last_name: addOnlyLetters(addMinAndMaxNotEmpty(Yup.string(), 'last_name', 2, 254)),
-  password: addMinAndMaxNotEmpty(Yup.string(), 'password', 5, 255),
-  confirmPassword: getPasswordConfirmSchema(),
-  email: getEmailSchema().nullable(),
-  phone: getPhoneSchema().nullable(),
-  birth_date: getStringDateSchema().nullable(),
-  secret_question: addMinAndMaxNotEmpty(Yup.string(), 'secret_question', 0, 255),
-  secret_question_answer: addMinAndMaxNotEmpty(Yup.string(), 'secret_question_answer', 2, 255),
-  secret_question_custom_text: Yup.string().when(
-    'secret_question',
-    (value: string, schema: Yup.StringSchema) => {
+export default (): Yup.ObjectSchema<any> =>
+  Yup.object().shape({
+    first_name: addOnlyLetters(addMinAndMaxNotEmpty(Yup.string(), 'first_name', 2, 254)),
+    last_name: addOnlyLetters(addMinAndMaxNotEmpty(Yup.string(), 'last_name', 2, 254)),
+    password: addMinAndMaxNotEmpty(Yup.string(), 'password', 5, 255),
+    confirmPassword: getPasswordConfirmSchema(),
+    email: getEmailSchema().nullable(),
+    phone: getItlPhoneSchema().nullable(),
+    birth_date: getStringDateSchema().nullable(),
+    secret_question: addMinAndMaxNotEmpty(Yup.string(), 'secret_question', 0, 255),
+    secret_question_answer: addMinAndMaxNotEmpty(Yup.string(), 'secret_question_answer', 2, 255),
+    secret_question_custom_text: Yup.string().when('secret_question', (value: string, schema: Yup.StringSchema) => {
       return value === 'Autre'
-        ? schema.required(t.t('form_field_empty', {field: t.t('secret_question_answer')}))
+        ? schema.required(t.t('form_field_empty', { field: t.t('secret_question_answer') }))
         : schema.notRequired();
-    },
-  ),
-  centers: Yup.array(),
-});
+    }),
+    centers: Yup.array(),
+  });
