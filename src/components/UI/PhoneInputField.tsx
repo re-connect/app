36220978c740fe80b728f97d6e2bc27c6011dd66
Frédéric RAFFIN-CABOISSE <fraffinc@gmail.com>
2022-07-11 18:06:11 +0200

    feat: add itl number in profile update

--- src/components/UI/PhoneInputField.tsx
+++ src/components/UI/PhoneInputField.tsx
@@ -1,18 +1,33 @@
 import { Stack } from 'native-base';
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 import { StyleSheet, View } from 'react-native';
 import PhoneInput from 'react-native-phone-number-input';
 import { colors } from '../../style';
 import { TextFieldProps } from './TextField';
 import Icon from 'react-native-vector-icons/FontAwesome5';
-
+import parsePhoneNumber from 'libphonenumber-js';
 export type PhoneNumberType = Omit<TextFieldProps, 'fieldLabel'>;
 
 const PhoneInputField: React.FC<PhoneNumberType> = ({ handleChange, touched, value, error, okIcon }) => {
   const { t } = useTranslation();
 
   const phoneInput = useRef<PhoneInput>(null);
+  const [displayedValue, setDisplayedValue] = React.useState<string | null>(null);
+  const [defaultCode, setDefaultCode] = React.useState('FR');
+
+  useEffect(() => {
+    if (value && value !== '') {
+      const phoneNumber = parsePhoneNumber(value);
+
+      if (phoneNumber) {
+        setDisplayedValue(phoneNumber.nationalNumber);
+        phoneNumber.country && setDefaultCode(phoneNumber.country);
+      }
+    } else {
+      setDisplayedValue('');
+    }
+  }, [value]);
 
   const getRightIconName = () => {
     return !!value && !!okIcon && !!touched && !error ? 'check' : 'times';
@@ -32,18 +47,23 @@ const PhoneInputField: React.FC<PhoneNumberType> = ({ handleChange, touched, val
   return (
     <Stack mt={3} space={4} w="100%" backgroundColor={colors.white} borderRadius="24">
       <View style={[styles.wrapper, !!error && { borderColor: colors.red }]}>
-        <PhoneInput
-          filterProps={{ placeholder: t('choose_a_country'), style: { width: '100%' } }}
-          placeholder={t('phone')}
-          ref={phoneInput}
-          defaultValue={value}
-          defaultCode="FR"
-          layout="first"
-          onChangeFormattedText={handleChange}
-          textContainerStyle={styles.textContainer}
-          containerStyle={styles.container}
-        />
-        <RightElement />
+        {displayedValue !== null && (
+          <>
+            <PhoneInput
+              filterProps={{ placeholder: t('choose_a_country'), style: { width: '100%' } }}
+              placeholder={t('phone')}
+              ref={phoneInput}
+              defaultValue={displayedValue}
+              //@ts-ignore
+              defaultCode={defaultCode}
+              layout="first"
+              onChangeFormattedText={handleChange}
+              textContainerStyle={styles.textContainer}
+              containerStyle={styles.container}
+            />
+            <RightElement />
+          </>
+        )}
       </View>
     </Stack>
   );
