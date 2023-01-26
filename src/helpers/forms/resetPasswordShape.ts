ff24f8d3f2a8c2f7ebe5941d116600e5e593a666
Frédéric RAFFIN-CABOISSE <fraffinc@gmail.com>
2023-01-26 14:34:42 +0100

    fix: PR review use translation instead of hard french text for password error message

--- src/helpers/forms/resetPasswordShape.ts
+++ src/helpers/forms/resetPasswordShape.ts
@@ -1,12 +1,10 @@
 import * as Yup from 'yup';
 import { getPasswordConfirmSchema } from './yupHelpers';
+import t from '../../services/translation';
 
 const passwordRegex = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)[a-zA-Z0-9\W]{8,40}$/;
 export default (): Yup.ObjectSchema<any> =>
   Yup.object().shape({
-    password: Yup.string().matches(
-      passwordRegex,
-      "Le mot de passe doit comporter au minimum 8 caractères et maximum 40 ainsi qu'au moins une majuscule, une minuscule,un chiffre et un caractère spécial (?,!,$,&,@,#)",
-    ),
+    password: Yup.string().matches(passwordRegex, t.t('form_field_password_security_invalid')),
     confirm: getPasswordConfirmSchema(),
   });
