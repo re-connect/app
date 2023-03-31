ce0e0a8e03fa1b2f30854ea0597e9da5aa5ad814
Frédéric RAFFIN-CABOISSE <fraffinc@gmail.com>
2023-03-31 12:04:09 +0200

    feat: update upload buttons style

--- src/components/Documents/Components/ItemModal.tsx
+++ src/components/Documents/Components/ItemModal.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
 import { VStack } from 'native-base';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import { colors } from '../../../style';
@@ -11,23 +11,23 @@ const ItemModal: React.FC<{ label: string; onPress: () => void; iconName: string
 }) => {
   return (
     <TouchableOpacity onPress={onPress}>
-      <VStack
-        flexDirection={'row'}
-        justifyContent='center'
-        rounded='md'
-        bg={colors.darkGrayMoreTransparent}
-        m='1'
-        p='3'>
-        <Icon
-          color={colors.accent}
-          name={iconName}
-          size={20}
-          style={{ marginRight: 10, marginLeft: 5, alignSelf: 'center' }}
-        />
-        <Text>{label}</Text>
+      <VStack flexDirection={'row'} justifyContent='center' rounded='md' bg={colors.primary} m='1' p='3'>
+        <Icon color={colors.white} name={iconName} size={20} style={styles.icon} />
+        <Text style={styles.label}>{label}</Text>
       </VStack>
     </TouchableOpacity>
   );
 };
+
+const styles = StyleSheet.create({
+  icon: {
+    marginRight: 10,
+    marginLeft: 5,
+    alignSelf: 'center',
+  },
+  label: {
+    color: colors.white,
+  },
+});
 
 export default ItemModal;
