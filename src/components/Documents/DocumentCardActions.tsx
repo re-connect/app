cb4ade75283b1f488928d320c76552b8e589e569
Frédéric RAFFIN-CABOISSE <fraffinc@gmail.com>
2023-07-05 17:09:36 +0200

    fix: modals issue on android

--- src/components/Documents/DocumentCardActions.tsx
+++ src/components/Documents/DocumentCardActions.tsx
@@ -1,10 +1,9 @@
 import * as React from 'react';
-import { useBoolean } from 'react-hanger/array';
-import { Modal, StyleSheet, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import { colors } from '../../style';
 import { DocumentInterface } from '../../types/Documents';
-import DocumentActionsModal from './DocumentActionsModal';
+import { UseBooleanActions } from 'react-hanger/array';
 
 const styles = StyleSheet.create({
   icon: {
@@ -17,19 +16,23 @@ const styles = StyleSheet.create({
 
 interface DocumentCardActionsProps {
   document: DocumentInterface;
+  openModalActions: UseBooleanActions;
+  setCurrentDocument: (document: DocumentInterface) => void;
 }
 
-const DocumentCardActions: React.FC<DocumentCardActionsProps> = ({ document }) => {
-  const [isModalOpen, openModalACtions] = useBoolean(false);
-
-  React.useEffect(() => {}, [isModalOpen]);
+const DocumentCardActions: React.FC<DocumentCardActionsProps> = ({
+  document,
+  openModalActions,
+  setCurrentDocument,
+}) => {
+  const onPress = () => {
+    setCurrentDocument(document);
+    openModalActions.setTrue();
+  };
 
   return (
     <View style={{ position: 'relative' }}>
-      <Modal transparent visible={isModalOpen} animationType='fade'>
-        <DocumentActionsModal document={document} close={openModalACtions.setFalse}/>
-      </Modal>
-      <TouchableOpacity onPress={openModalACtions.setTrue} style={styles.icon}>
+      <TouchableOpacity onPress={onPress} style={styles.icon}>
         <Icon style={{ fontSize: 20 }} color={colors.black} name='ellipsis-v' />
       </TouchableOpacity>
     </View>
