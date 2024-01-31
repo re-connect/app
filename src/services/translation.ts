34880a0b718e2108b250d0932c5e53b31bfa336a
Frédéric RAFFIN-CABOISSE <fraffinc@gmail.com>
2024-01-31 15:57:49 +0100

    fix: i18 compatibility

--- src/services/translation.ts
+++ src/services/translation.ts
@@ -39,6 +39,7 @@ const resources = {
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
+    compatibilityJSON: 'v3',
     initImmediate: true,
     resources,
     lng: 'gb',
