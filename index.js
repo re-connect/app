62fd30f5567da59c1492f2b9fa9464bc15645413
Thibaut Cheymol <thibaut.cheymol@gmail.com>
2024-02-22 16:29:10 +0100

    :wrench: [Expo] ⚠️ IMPORTANT - Migrate stack from RN to Expo

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-import { AppRegistry, YellowBox } from 'react-native';
+import { AppRegistry } from 'react-native';
 import 'react-native-gesture-handler';
 import App from './App';
 import { name as appName } from './app.json';
 
-YellowBox.ignoreWarnings(['`-[RCTRootView cancelTouches]`']);
 AppRegistry.registerComponent(appName, () => App);
